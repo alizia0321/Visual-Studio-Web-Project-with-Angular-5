5a74d4854525e8cfb005bb1b6407ead4b31960a6
Carlo Roncallo <roncalw@hotmail.com>
2018-02-13 10:48:47 -0500

    Add project files.

---
+++ WebApplication1/dist/out-tsc/src/app/app.component.js
@@ -0,0 +1,26 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title1 = 'Angular!';
+        this.title2 = 'This is a CLI Project merged with an ASP.NET Web Application (.NET Framework) Project, using an Empty template';
+        this.title3 = 'No check box was checked to "Add folders and core references for:" Web Forms, MVC or Web API';
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app-root',
+            templateUrl: './app.component.html',
+            styleUrls: ['./app.component.css']
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ WebApplication1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title1 = 'Angular!';
+
+  title2 = 'This is a CLI Project merged with an ASP.NET Web Application (.NET Framework) Project, using an Empty template';
+
+  title3 = 'No check box was checked to "Add folders and core references for:" Web Forms, MVC or Web API';
+}
